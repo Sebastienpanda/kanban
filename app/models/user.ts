d256cef85ef4db912517183c49ac4255858b76fe
Sebastienpanda <sebastienweb27@gmail.com>
2024-12-21 01:03:40 +0100

    feat: new model users

--- app/models/user.ts
+++ app/models/user.ts
@@ -1,8 +1,11 @@
-import { DateTime } from 'luxon'
-import hash from '@adonisjs/core/services/hash'
-import { compose } from '@adonisjs/core/helpers'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
+import { compose } from '@adonisjs/core/helpers'
+import hash from '@adonisjs/core/services/hash'
+import { BaseModel, column, hasMany, hasOne } from '@adonisjs/lucid/orm'
+import type { HasMany, HasOne } from '@adonisjs/lucid/types/relations'
+import { DateTime } from 'luxon'
+import { UserRole } from '../enums/user_role.js'
+import Label from './label.js'
 
 const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
   uids: ['email'],
@@ -11,10 +14,7 @@ const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
 
 export default class User extends compose(BaseModel, AuthFinder) {
   @column({ isPrimary: true })
-  declare id: number
-
-  @column()
-  declare fullName: string | null
+  declare id: string
 
   @column()
   declare email: string
@@ -22,9 +22,39 @@ export default class User extends compose(BaseModel, AuthFinder) {
   @column({ serializeAs: null })
   declare password: string
 
+  @column()
+  declare pseudo: string
+
+  @column()
+  declare avatar?: string
+
+  @column({
+    consume: (value: string) => value as UserRole,
+    prepare: (value: UserRole) => value,
+  })
+  declare role: UserRole
+
+  @column()
+  declare isActive: boolean
+
+  @hasMany(() => Label)
+  declare labels: HasMany<typeof Label>
+
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
 
+  @hasOne(() => User)
+  declare createdBy: HasOne<typeof User>
+
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime | null
+
+  @hasOne(() => User)
+  declare updatedBy: HasOne<typeof User>
+
+  @column.dateTime()
+  declare deletedAt: DateTime | null
+
+  @hasOne(() => User)
+  declare deletedBy: HasOne<typeof User>
 }
--- database/migrations/1734693305650_create_users_table.ts
+++ database/migrations/1734693305650_create_users_table.ts
@@ -1,21 +1,41 @@
 import { BaseSchema } from '@adonisjs/lucid/schema'
+import { UserRole } from '../../app/enums/user_role.js'
 
 export default class extends BaseSchema {
   protected tableName = 'users'
 
   async up() {
+    await this.raw('create extension IF NOT EXISTS "uuid-ossp" schema pg_catalog version "1.1";')
+
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').notNullable()
-      table.string('full_name').nullable()
+      table
+        .uuid('id', { primaryKey: true })
+        .defaultTo(this.db.rawQuery('uuid_generate_v4()').knexQuery)
+
       table.string('email', 254).notNullable().unique()
       table.string('password').notNullable()
 
+      table.string('pseudo', 50).notNullable()
+
+      table.string('avatar', 255).nullable()
+
+      table.enum('role', Object.values(UserRole)).defaultTo(UserRole.USER).notNullable()
+
+      table.boolean('is_active').defaultTo(false).notNullable()
+
       table.timestamp('created_at').notNullable()
+      table.uuid('created_by').notNullable().references('id').inTable('users')
+
       table.timestamp('updated_at').nullable()
+      table.uuid('updated_by').nullable().references('id').inTable('users')
+
+      table.timestamp('deleted_at').nullable()
+      table.uuid('deleted_by').nullable().references('id').inTable('users')
     })
   }
 
   async down() {
     this.schema.dropTable(this.tableName)
+    this.schema.raw('DROP EXTENSION IF EXISTS "uuid-ossp"')
   }
 }
